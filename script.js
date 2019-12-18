4ca9237d33a51549d6c6f3fe1faf03a1eae5881d
cpsavva <csoriasavva@gmail.com>
2019-12-18 16:19:19 -0500

    attempting to put data attributes into an array to see if i can loop through inputs to display without a lot of code

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-function takeWord() {
-		console.log("did this work right?" + $(".preview").val() );
-		$("#word").html('<span class="spanbox">'+$(".preview").val()+'</span>')
-	};
+function arrangeWord() {
 
-function displayWord() {
-	$(".lalalu").click(function(){takeWord()})
-
+	// console.log("did this work right?" + $("[data-grammar]").val() );
+	// $("#word").html('<span class="spanbox">'+$("[data-grammar]").val()+'</span>')
 };
 
+var grammarArr = []
+function extractWord(){
 
+	$('[data-grammar]').each(function() {
+  	console.log($(this).data('grammar'))
+  	grammarArr.push($(this).data('grammar'))
+  })
+  	console.log(grammarArr)
+};
 
+function displayWord() {
+	// $(".lalalu").click(function(){takeWord()})
+	$(".lalalu").click(function(){
+		extractWord();
+		arrangeWord();
+	}); 
+};
 
 
 $(document).ready(
