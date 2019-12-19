933afb4a91326c57c25f9bafbf7a1da68a7c0c68
cpsavva <csoriasavva@gmail.com>
2019-12-19 14:21:18 -0500

    added file to test carousel effect of the form

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 var grammarArr = []
+var wordArr = []
 //function to push data attributes to an array to get the 
 function extractDataAttr(){
 
-	$('[data-grammar]').each(function(name) {
+	$('[data-grammar]').each(function() {
   	console.log($(this).data('grammar'))
   	grammarArr.push($(this).data('grammar'))
   })
+	$('[data-word]').each(function(){
+		console.log($(this).data('word'))
+  	wordArr.push($(this).data('word'))
+	})
   	console.log(grammarArr)
+  	console.log(wordArr)
 };
 
 //function to place the inputs in their prospective places throughout the Mad Lib
@@ -14,28 +20,32 @@ function arrangeWord() {
 	$.each(grammarArr, function(index, val){
 		$("#"+val).html('<span class="spanbox">' + $("input[data-grammar="+ val+ "]").val() + '</span>')
 	})
-	grammarArr = [];
-	console.log(grammarArr);
+	// grammarArr = [];
+	// console.log(grammarArr);
 };
 
 //function to do the above actions and clear input fields and arrays.
 function displayWord() {
-		extractDataAttr();
+		// extractDataAttr();
 		arrangeWord();
 		$(":input").val('');
-		$(".madlib").hide();
+		$(".madLib").hide();
 		$(".poem").show();
 };
+//function to start the mad lib again
 function restart(){
 	$(".startAgain").click(function(){
 		$(".poem").hide();
-		$(".madlib").show();
+		$(".madLib").show();
 	})
 }
 
 
+//function to slide show the form?
+
 $(document).ready(
 	$(".poem").hide())
+	extractDataAttr();
 	$(".finish").click(function(){
 	displayWord()
 	restart()
