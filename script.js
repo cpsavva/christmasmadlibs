bccaa35d8b4c43d47a0078b9949852db2ac7eec5
cpsavva <csoriasavva@gmail.com>
2019-12-19 12:10:23 -0500

    fixed my function problem with the input and put in resets for the input fields and array

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 var grammarArr = []
+//function to push data attributes to an array to get the 
 function extractDataAttr(){
 
-	$('[data-grammar]').each(function() {
+	$('[data-grammar]').each(function(name) {
   	console.log($(this).data('grammar'))
   	grammarArr.push($(this).data('grammar'))
   })
   	console.log(grammarArr)
 };
 
+//function to place the inputs in their prospective places throughout the Mad Lib
 function arrangeWord() {
-	console.log("arrangWord function")
-	console.log("this is the array " + grammarArr)
+	// console.log("arrangWord function")
+	// console.log("this is the array " + grammarArr)
 	$.each(grammarArr, function(index, val){
-		console.log("this is an "+ val)
-		$("#"+val).html('<span class="spanbox">' + $("[data-grammar."+val+"]").val() + '</span>')
+		$("#"+val).html('<span class="spanbox">' + $("input[data-grammar="+ val+ "]").val() + '</span>')
 	})
-	// console.log("did this work right?" + $("[data-grammar]").val() );
-	// $("#word").html('<span class="spanbox">'+$("[data-grammar]").val()+'</span>')
+	grammarArr = [];
+
+	console.log(grammarArr);
 };
 
 
 
 function displayWord() {
-	// $(".lalalu").click(function(){takeWord()})
-	$(".lalalu").click(function(){
+	$(".submit").click(function(){
 		extractDataAttr();
 		arrangeWord();
+			$(':input').val('');
 	}); 
 };
 
